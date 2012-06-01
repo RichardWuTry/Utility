e81a5c4e4042f54df536e5465ba18ff83077b9ab
DBFocus <shadow_wu82@163.com>
2012-06-01 17:24:37 +0800

    update Exam app

--- Lib/Action/QuestionAction.class.php
+++ Lib/Action/QuestionAction.class.php
@@ -1,11 +1,11 @@
 <?php
 class QuestionAction extends Action {	
 	public function add() {
-		if (!isset($_GET['p'])) {
-			redirect_to(__APP__.'/Paper/create');
-			return;
-		}
-		$paper_id = $_GET['p'];
+		// if (!isset($_GET['p'])) {
+			// redirect_to(__APP__.'/Paper/create');
+			// return;
+		// }
+		$paper_id = isset($_GET['p']) ? $_GET['p'] : 0;
 		
 		$totalQ = 0;
 		$PaperQuestion = M('PaperQuestion');
@@ -26,23 +26,55 @@ class QuestionAction extends Action {
 		$this->display();
 	}
 	
-	public function save() {
+	public function save() {		
 		if ($this->isPost()) {
 			$QuestionHead = D('QuestionHead');
-			if ($QuestionHead->create()) {
+			if ($QuestionHead->create()) {				
+				$QuestionHead->startTrans();				
+				$flag = true;				
+				if (!$QuestionHeadId = $QuestionHead->add()) {
+					$flag = false;
+				}
 				
-				$QuestionHead->startTrans();
+				if ($flag) {
+					$QuestionDetail = M('QuestionDetail');
+					$currDateTime = date("Y-m-d H:i:s");
+					$i = 1;
+					$options = $_POST['option'];
+					while (isset($_POST[$i])) {
+						$data[$i-1]['question_id'] = $QuestionHeadId;
+						$data[$i-1]['item_name'] = $_POST[$i];
+						if (in_array($i, $options)) {
+							$data[$i-1]['correct_value'] = '1';
+						} else {
+							$data[$i-1]['correct_value'] = '0';
+						}
+						$data[$i-1]['create_at'] = $currDateTime;
+						$i++;
+					}
+					if (!$QuestionDetail->addAll($data)) {
+						$flag = false;					
+					}
+				}				
 				
-				if ($QuestionHeadId = $QuestionHead->add()) {
-					
-					
+				if ($flag && isset($_POST['paper_id'])) {
+					$PaperQuestion = M('PaperQuestion');
+					$pqData['paper_id'] = $_POST['paper_id'];
+					$pqData['question_id'] = $QuestionHeadId;
+					$pqData['question_seq'] = $_POST['question_seq'];
+					$pqData['create_at'] = $currDateTime;
+					if (!$PaperQuestion->add($pqData)) {
+						$flag = false;
+					}
+				}				
+				
+				if ($flag) {				
 					$QuestionHead->commit();
-					$this->success($QuestionHeadId);
+					$this->success($_POST['eid']);
 				} else {
 					$QuestionHead->rollback();
 					$this->error('写入数据库错误');
-				}
-				
+				}				
 			} else {
 				$this->error($QuestionHead->getError());
 			}
--- Lib/Model/QuestionDetailModel.class.php
+++ Lib/Model/QuestionDetailModel.class.php
@@ -1,5 +1,5 @@
 <?php
 class QuestionDetailModel extends Model {
-
+	
 }
 ?>
