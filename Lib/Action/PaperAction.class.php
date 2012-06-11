e78f397305de6d5b87c0e65bc4775f4fd0df8d9b
DBFocus <shadow_wu82@163.com>
2012-06-11 17:33:52 +0800

    update Exam app

--- Lib/Action/PaperAction.class.php
+++ Lib/Action/PaperAction.class.php
@@ -20,5 +20,39 @@ class PaperAction extends Action {
 			redirect_to(__URL__.'/create');
 		}
 	}
+	
+	public function review() {
+		if (!isset($_GET['p'])) {
+			$this->error('页面错误');
+		}
+		
+		$paperId = $_GET['p'];
+		$ExamPaper = M('ExamPaper');
+		if ($paper = $ExamPaper
+					-> where("paper_id = $paperId")
+					-> field("paper_id, paper_name, total_score, total_mins, paper_desc")
+					-> find()) {
+			$Model = M();
+			if ($question = $Model->query("select
+											pq.question_seq,
+											pq.question_id,
+											qh.question_name,
+											qh.question_type,
+											qh.question_score
+										from
+											paper_question pq
+											join
+											question_head qh
+											on
+												pq.question_id = qh.question_id
+										where
+											paper_id = $paperId")) {
+				$this->assign('question', $question);				
+			}
+			$this->assign('paper', $paper);
+		}
+		
+		$this->display();
+	}
 }
 ?>
