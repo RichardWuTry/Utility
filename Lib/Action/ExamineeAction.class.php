1137c595e48e9d73f43d341eb62f85bcc5334c7c
DBFocus <shadow_wu82@163.com>
2012-07-02 17:42:12 +0800

    add attend page

--- Lib/Action/ExamineeAction.class.php
+++ Lib/Action/ExamineeAction.class.php
@@ -12,15 +12,32 @@ class ExamineeAction extends Action {
 						->where('exam_id = '.$tokenData['examId'])
 						->field('exam_id, paper_id, open_datetime, close_datetime, link')
 						->find()) {
+			//比对数据库中的token与链接中的token
 			if ($currExam['link'] != $token) {
 				$this->error('页面错误');
 			}
 			
+			//验证考试是否开放
 			$today = date("Y-m-d H:i:s");
 			if ($today < $currExam['open_datetime']) {
 				$this->error('考试还未开放，请确认开考时间');
 			} else if ($today > $currExam['close_datetime']) {
 				$this->error('考试已关闭');
+			}
+			
+			//显示考卷的卷头信息
+			$ExamPaper = M('ExamPaper');
+			if ($paper = $ExamPaper
+						->where('paper_id = '.$currExam['paper_id'])
+						->field('paper_id, paper_name, total_score, total_mins, paper_desc')
+						->find()) {
+				//存储问题到Session中						
+				
+						
+				$this->assign('paper', $paper);
+				$this->display();
+			} else {
+				$this->error('页面错误');
 			}
 		} else {
 			$this->error('页面错误');
